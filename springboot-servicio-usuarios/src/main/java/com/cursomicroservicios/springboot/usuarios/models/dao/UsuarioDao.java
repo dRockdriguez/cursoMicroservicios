b41239b0ff0c8e9caa1630d5e2e0e59b73f1e7ba
David Rodriguez <david.rodriguez@grupoonetec.com>
2020-02-13 15:34:55 +0100

    .

---
+++ springboot-servicio-usuarios/src/main/java/com/cursomicroservicios/springboot/usuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+package com.cursomicroservicios.springboot.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.cursomicroservicios.springboot.usuarios.models.entity.Usuario;
+
+@RepositoryRestResource(path = "usuarios")
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
+	@RestResource(path = "buscar-username")
+	Usuario findByUsername(String username);
+
+	@Query("SELECT u FROM Usuario u WHERE u.username = ?1")
+	Usuario buscarPorUsername(String username);
+}
